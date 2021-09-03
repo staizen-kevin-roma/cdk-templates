b0060437efdae69119e27a58a64fe96bf959aac2
Kevin Roma <89840116+staizen-kevin-roma@users.noreply.github.com>
2021-09-03 17:28:56 +0800

    Update cloudfront.ts - test sonar quality gate

--- cloudfront/bin/cloudfront.ts
+++ cloudfront/bin/cloudfront.ts
@@ -4,6 +4,6 @@ import * as cdk from '@aws-cdk/core';
 import { CloudfrontStack } from '../lib/cloudfront-stack';
 
 const app = new cdk.App();
-(app, 'CloudfrontStack', {
+new CloudfrontStack(app, 'CloudfrontStack', {
     description: 'The AWS Cloudformation template for AppSync and API gateway Cloudfronts'
 });
